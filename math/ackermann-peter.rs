654fd752f55dd0f61f6c3988a8267cc6cde06e23
NerdyPepper <akshayoppiliappan@gmail.com>
2018-10-20 12:36:33 +0530

    Add Ackermann-Peter function

---
+++ math/ackermann-peter.rs
@@ -0,0 +1,15 @@
+fn main() {
+    println!("{}", ackermann(3, 4));
+}
+
+fn ackermann(m: i128, n: i128) -> i128 {
+    if m == 0{
+        n + 1
+    } else if m > 0 && n == 0 {
+        ackermann(m - 1, 1)
+    } else if m > 0 && n > 0 {
+        ackermann(m - 1, ackermann(m, n - 1))
+    } else {
+        0i128
+    }
+}
