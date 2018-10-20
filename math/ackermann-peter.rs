f29e09f1806bfd17c7453e55dc63905f2a537d6e
NerdyPepper <akshayoppiliappan@gmail.com>
2018-10-20 12:38:21 +0530

    Add output samples

--- math/ackermann-peter.rs
+++ math/ackermann-peter.rs
@@ -1,5 +1,6 @@
 fn main() {
-    println!("{}", ackermann(3, 4));
+    println!("{}", ackermann(1, 1)); // 3
+    println!("{}", ackermann(3, 4)); // 125
 }
 
 fn ackermann(m: i128, n: i128) -> i128 {
