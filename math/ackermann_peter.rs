afe82008058c234b4eb756acabc31fd7b9876530
NerdyPepper <akshayoppiliappan@gmail.com>
2019-01-13 22:35:20 +0530

    Use unsigned

--- math/ackermann_peter.rs
+++ math/ackermann_peter.rs
@@ -3,7 +3,7 @@ fn main() {
     println!("{}", ackermann(3, 4)); // 125
 }
 
-fn ackermann(m: i128, n: i128) -> i128 {
+fn ackermann(m: u128, n: u128) -> u128 {
     if m == 0{
         n + 1
     } else if m > 0 && n == 0 {
@@ -11,6 +11,6 @@ fn ackermann(m: i128, n: i128) -> i128 {
     } else if m > 0 && n > 0 {
         ackermann(m - 1, ackermann(m, n - 1))
     } else {
-        0i128
+        0u128
     }
 }
