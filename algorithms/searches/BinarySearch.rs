b8cf1e8baf213f059ec5a5f14d437ffe87873618
Zalan <toth.zalan03@gmail.com>
2020-10-09 18:12:21 +0200

    Added searching algorithms
    Added Binary and Linear search

---
+++ algorithms/searches/BinarySearch.rs
@@ -0,0 +1,24 @@
+pub fn binary_search(array: &[i32], item: i32) -> i32 {
+    let mut high: i32 = array.len() as i32 - 1;
+    let mut low: i32 = 0;
+    let mut mid: i32 = 0;
+	while low <= high {
+		mid = ((high - low) / 2) + low;
+		let val = array[mid as usize];
+		if val == item {
+			return mid as i32;
+		}
+		if val > item {
+			high = mid - 1;
+		}
+		if val < item {
+			low = mid + 1;
+		}	
+	}
+	return -1;
+}
+
+fn main() {
+    let arr = [ 1, 2, 5, 11, 12, 15, 21, 22, 25 ];
+    println!("{}",binary_search(&arr, 15));
+}
